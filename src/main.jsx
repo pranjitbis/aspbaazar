8b94e50e3b4662aea5b35ef612f126e7ee2182b1
pranjitbis <Pranjitbiswas77@gmail.com>
2026-02-16 12:31:48 +0530

    s

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import About from "./Componet/About/About.jsx";
-import Contact from "./Componet/Contact/Contact.jsx"
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-]);
+import Contact from "./Componet/Contact/Contact.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 );
